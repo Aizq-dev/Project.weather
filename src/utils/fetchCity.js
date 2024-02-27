ee219a4817cebfea4723e7cad3c54fe0bb403f2c
Ragnarok794 <adrianizq94@gmail.com>
2024-02-27 17:18:35 +0100

    fixing netlify deploy

--- src/utils/fetchCity.js
+++ src/utils/fetchCity.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const Api_Key = '9b88334e127b5110b6f05a04bf7a306b'
-const URL_API = 'http://api.openweathermap.org/geo/1.0/direct?q='
+const URL_API = 'https://api.openweathermap.org/geo/1.0/direct?q='
 
 export const fetchCity = async (query,setCities) => {
 
